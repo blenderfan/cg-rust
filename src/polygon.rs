b181c974bc40641435cc020a224f5048c0a5ee3b
blenderfan <blenderfan@gmx.at>
2025-11-08 16:49:25 +0100

    polygon documentation

--- src/polygon.rs
+++ src/polygon.rs
@@ -1,4 +1,9 @@
-
+//! # Polygons
+//! 
+//! Polygons consist of a list of points in the plane, which are sequentially connected by
+//! line segments, forming a bounded area in this way. A choice can be made to list the vertices
+//! clockwise (CW) or counter-clockwise (CCW). All polygons in this package are assumed to be
+//! CCW, while holes in a polygon are assumed to be CW.
 
 use core::f64;
 
@@ -6,6 +11,15 @@ use crate::vector::Vec2;
 use crate::vector::Vec2d;
 
 pub trait PolygonFloat<T : Vec2, ElementType> {
+    
+    /// Generates a regular polygon where each line segment has the same length and the
+    /// angle between all edges are equal as well.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// let poly = Polygon::Vec2d::regular(Vec2d::new(0.5, 0.5), 0.5, 6);
+    /// ```
     fn regular(center : T, radius : ElementType, corners : i32) -> Polygon<T>;
 }
 
@@ -27,6 +41,17 @@ impl<T: Vec2> Polygon<T> {
         return &self.points;
     }
 
+    /// Adds one vertex to the end of the list of vertices of the polygon.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// let mut poly = Polygon::Vec2f::new();
+    /// poly.push(Vec2f::new(0.0, 0.0));
+    /// poly.push(Vec2f::new(1.0, 0.0));
+    /// poly.push(Vec2f::new(1.0, 1.0));
+    /// poly.push(Vec2f::new(0.0, 1.0));
+    /// ```
     pub fn push(&mut self, point : T) {
         self.points.push(point);
     }
@@ -34,6 +59,7 @@ impl<T: Vec2> Polygon<T> {
 }
 
 impl<> PolygonFloat<Vec2d, f64> for Polygon<Vec2d> {
+
 
     fn regular(center : Vec2d, radius : f64, corners : i32) -> Polygon<Vec2d> {
         
--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,4 @@
 
-
 use std::fmt;
 
 use std::ops::Add;
