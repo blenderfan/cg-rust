68a342d7e3c5a14521029d2c205c326528a65172
blenderfan <blenderfan@gmx.at>
2025-12-04 00:57:13 +0100

    export freeing memory

--- autogen/cg_rust.h
+++ autogen/cg_rust.h
@@ -4,18 +4,24 @@
 #include <ostream>
 #include <new>
 
+struct CVec2f {
+  float x;
+  float y;
+};
+
 template<typename T>
 struct PArray {
   const T *data;
   size_t size;
 };
 
-struct CVec2f {
-  float x;
-  float y;
-};
-
 extern "C" {
+
+void cg_rust_free_array_float2(PArray<CVec2f> *a);
+
+void cg_rust_free_array_long(PArray<size_t> *a);
+
+void cg_rust_free_array_int(PArray<int> *a);
 
 PArray<size_t> *cg_rust_polygon_triangulate(const PArray<CVec2f> *points);
 
--- src/c_export/c_arr.rs
+++ src/c_export/c_arr.rs
@@ -1,5 +1,7 @@
 
 use libc::size_t;
+use libc::c_int;
+use crate::c_export::c_vector::CVec2f;
 
 #[repr(C)]
 pub struct PArray<T> {
@@ -15,6 +17,28 @@ impl<T> PArray<T> {
             size: size
         }
     }
-    
+}
 
+#[unsafe(no_mangle)]
+pub extern "C" fn cg_rust_free_array_float2(a: *mut PArray<CVec2f>) {
+    if !a.is_null() {
+        return;
+    }
+    let _b = unsafe { Box::from_raw(a)};
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn cg_rust_free_array_long(a: *mut PArray<size_t>) {
+    if !a.is_null() {
+        return;
+    }
+    let _b = unsafe { Box::from_raw(a)};
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn cg_rust_free_array_int(a: *mut PArray<c_int>) {
+    if !a.is_null() {
+        return;
+    }
+    let _b = unsafe { Box::from_raw(a)};
 }
